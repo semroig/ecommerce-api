2416525e7573888c8bd34f7bbbde3cbf8dc3af6e
Sem Gabriel Roig <sem.roig@gmail.com>
2023-04-24 19:40:19 -0300

    leo docu de typing y armo rutas simples

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+# A partir de aca API posta
+
+@app.get("/products")
+def read_products(q: Union[str, None] = None):
+    return {"Hello": "World"}
+
+@app.get("/products/{item_id}")
+def read_product(item_id: int):
+    return {"Hello": "World"}
+
+@app.get("/brands")
+def read_brands():
+    return {"Hello": "World"}
+
+@app.get("/categories")
+def read_categories():
+    return {"Hello": "World"}
